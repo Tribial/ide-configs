4a4a8ef7d855ee64109e2eaab6d5e55b010428d5
fdomurad <Fabian.Domurad2@nestbank.pl>
2025-08-01 12:03:29 +0200

    added zsh, zellij, updated rest

---
+++ install/apply_config.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ -n "$BASH_SOURCE" ]; then
+	SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
+elif [ -n "$ZSH_VERSION" ]; then
+	SCRIPT_PATH="$(realpath "${(%):-%N}")"
+else
+	echo "Unsupported shell"
+	exit 1
+fi
+DOTFILES_DIR="$(dirname "$(dirname "$SCRIPT_PATH")")"
+
+declare -A SYMLINKS=(
+	["$DOTFILES_DIR/ZSH/.zshrc"]="$HOME/.zshrc"
+	["$DOTFILES_DIR/Zellij"]="$HOME/.config/zellij"
+	["$DOTFILES_DIR/NeoVim"]="$HOME/.config/nvim"
+)
+
+create_symlink() {
+	local src="$1"
+	local dest="$2"
+
+	if [ -L "$dest" ]; then
+		echo "Replacing symlink: $dest"
+		ln -sf "$src" "$dest"
+		echo "Replaced symlink: $dest -> $src"
+
+	elif [ -e "$dest" ]; then
+		echo "$dest exists and is not a symlink. Removing it."
+		rm -rf "$dest"
+		ln -s "$src" "$dest"
+		echo "Creates new symlink: $dest -> $src"
+	else
+		echo "Creating symlink: $dest"
+		ln -s "$src" "$dest"
+		echo "Creates new symlink: $dest -> $src"
+	fi
+}
+
+for src in "${!SYMLINKS[@]}"; do
+	dest="${SYMLINKS[$src]}"
+	create_symlink "$src" "$dest"
+done
+
+echo "Config symlinks applied."
